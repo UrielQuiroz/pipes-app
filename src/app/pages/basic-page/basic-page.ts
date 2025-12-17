479042b739598bd09f3e115220ac795c386effe9
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-17 17:15:59 -0600

    Date Pipe

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,9 +1,11 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [ LowerCasePipe, UpperCasePipe, TitleCasePipe ],
+  imports: [
+    LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe
+  ],
   templateUrl: './basic-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -11,4 +13,19 @@ export default class BasicPage {
   nameLower = signal('uriel');
   nameUpper = signal('URIEL');
   fullName = signal('uRiEl QuIrOz');
+
+  customDate = signal( new Date() )
+
+  tickingDateEffect = effect((onCleanup) => {
+
+    const interval = setInterval(() => {
+      this.customDate.set( new Date() );
+      console.log('tick')
+    }, 1000);
+
+    onCleanup(() => {
+      clearInterval(interval)
+    })
+
+  })
 }
