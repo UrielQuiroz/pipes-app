d4ea1873e8e794f4de62739c13f7fbdd8dabc8bf
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-17 16:26:04 -0600

    UpperCase, LowerCase y TitleCase Pipes

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,9 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [ LowerCasePipe, UpperCasePipe, TitleCasePipe ],
   templateUrl: './basic-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BasicPage { }
+export default class BasicPage {
+  nameLower = signal('uriel');
+  nameUpper = signal('URIEL');
+  fullName = signal('uRiEl QuIrOz');
+}
