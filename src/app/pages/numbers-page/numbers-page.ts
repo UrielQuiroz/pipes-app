180ff438f4a2e55f88833b74537d6424e44da3cd
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-17 16:48:01 -0600

    Decimal, Percent y Currency Pipes

--- src/app/pages/numbers-page/numbers-page.ts
+++ src/app/pages/numbers-page/numbers-page.ts
@@ -1,9 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [ DecimalPipe, PercentPipe, CurrencyPipe ],
   templateUrl: './numbers-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class NumbersPage { }
+export default class NumbersPage {
+  totalSells = signal(2_433_232.5567);
+  percent = signal(0.5642);
+ }
