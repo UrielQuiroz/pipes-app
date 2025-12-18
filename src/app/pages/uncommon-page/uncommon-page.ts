18dc6e6919d35d19b9c0d8d1ae8b3df4afea931e
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-17 22:24:58 -0600

    I18nSelectPipe

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,10 +1,44 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Uriel',
+  gender: 'male',
+  age: 30,
+  address: 'Nuevo Leon, México'
+}
+
+const client2 = {
+  name: 'Carolina',
+  gender: 'female',
+  age: 30,
+  address: 'Sinaloa, México'
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class UncommonPage { }
+export default class UncommonPage {
+
+  // i18Select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient() {
+    if( this.client() == client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
+
+}
