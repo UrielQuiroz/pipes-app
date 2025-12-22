e11b46fa2b6a954a571b4006cf44b881705bfcc5
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-22 14:53:38 -0600

    implementacion de interfaz de datos, consumir arreglo de información y renderizar tabla

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { toggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
 
 @Component({
   selector: 'app-custom-page',
@@ -9,6 +10,7 @@ import { toggleCasePipe } from '../../pipes/toggle-case.pipe';
 })
 export default class CustomPage {
   name = signal('Uriel Quiroz')
-
   upperCase = signal(false);
+
+  heroes = signal(heroes);
 }
