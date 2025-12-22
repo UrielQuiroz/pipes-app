55eb9fbffd48e223422a7ecdb2ca1c649a7dee11
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-22 16:19:24 -0600

    Pipe personalizado - HeroTextColor

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -3,10 +3,13 @@ import { toggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { heroColorPipe } from '../../pipes/color.pipe';
+import { heroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
+import { heroCreatorPipe } from '../../pipes/hero-creator.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ toggleCasePipe, CanFlyPipe, heroColorPipe ],
+  imports: [ toggleCasePipe, CanFlyPipe, heroColorPipe, heroTextColorPipe, TitleCasePipe, heroCreatorPipe ],
   templateUrl: './custom-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
